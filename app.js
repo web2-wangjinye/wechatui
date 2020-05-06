92c70abfa14e8cb53226c5c3eba5f02ec3133daa
wangjinye <2990866946@qq.com>
2020-05-06 17:59:54 +0800

    测试git3

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-    console.log(2222222222)
+    console.log("2222ddd222222")
     // 登录
     wx.login({
       success: res => {
