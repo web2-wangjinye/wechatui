c42c0032a219782c23d7fb7998ff849c7af6065f
wangjinye <2990866946@qq.com>
2020-05-06 18:03:38 +0800

    git清除测试

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-    console.log("2222dddddd222222")
     // 登录
     wx.login({
       success: res => {
