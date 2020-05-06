2e314590d58b4f09029ee90389c6ef03098134e5
web2-wangjinye <2990866946@qq.com>
2020-05-06 21:11:08 +0800

    新增授权登录页

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 //app.js
 App({
   onLaunch: function () {
-    console.log(111111)
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
--- pages/authorize/authorize.js
+++ pages/authorize/authorize.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    canIUse: wx.canIUse('button.open-type.getUserInfo')//获取用户信息是否在当前版本可用
   },
 
   /**
@@ -62,5 +62,33 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+   /**
+   * 点击授权按钮事件
+   */
+  bindGetUserInfo: function (e) {//点击的“拒绝”或者“允许
+    if(e.detail.userInfo){//点击了“允许”按钮，
+      var that=this;
+      requestUrl.requestUrl({//将用户信息传给后台数据库
+        url: "/QXEV/xxx/xxx",
+        params: {
+          openId: globalOpenId,//用户的唯一标识
+          nickName: e.detail.userInfo.nickName,//微信昵称
+          avatarUrl: e.detail.userInfo.avatarUrl,//微信头像
+          province: e.detail.userInfo.province,//用户注册的省
+          city: e.detail.userInfo.city//用户注册的市
+        },
+        method: "post",
+      })
+        .then((data) => {//then接收一个参数，是函数，并且会拿到我们在requestUrl中调用resolve时传的的参数
+            console.log("允许授权了");
+        })
+        .catch((errorMsg) => {
+          console.log(errorMsg)
+        })
+        wx.switchTab({
+          url: '../index/index',
+        })
+    }
   }
 })
