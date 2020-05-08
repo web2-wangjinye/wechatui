9b6e6ae62b8d1744b9ee33352bcdcff189651236
web2-wangjinye <2990866946@qq.com>
2020-05-08 22:47:47 +0800

    滚动加载

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,10 +4,11 @@ const app = getApp()
 const $api = require('../../utils/request.js').API;
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    height: '',
+    res: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    searchLoading: false, //"上拉加载"的变量，默认false，隐藏  
+    searchLoadingComplete: false  //“没有数据”的变量，默认false，隐藏 
   },
   //事件处理函数
   goDetail: function() {
@@ -26,7 +27,41 @@ Page({
       this.logo.showDialog();//调用子组件的方法
     }
   },
-  onLoad: function () {
+  lower() {
+    var result = this.data.res;
 
+    var resArr = [];
+    for (let i = 0; i < 10; i++) {
+      resArr.push(i);
+    };
+    var cont = result.concat(resArr);
+    console.log(resArr.length);
+    if (cont.length >= 30) {
+      this.setData({  
+        searchLoadingComplete: true, //把“没有数据”设为true，显示  
+        searchLoading: false  //把"上拉加载"的变量设为false，隐藏  
+      }); 
+      return false;
+    } else {
+      this.setData({  
+        searchLoadingComplete: false, //把“没有数据”设为true，显示  
+        searchLoading: true  //把"上拉加载"的变量设为false，隐藏  
+      }); 
+      setTimeout(() => {
+        this.setData({
+          res: cont
+        });
+        wx.hideLoading();
+      }, 1500)
+    }
+  },
+  onLoad: function () {
+    wx.getSystemInfo({
+      success: (res) => {
+        this.setData({
+          height: res.windowHeight
+        })
+      }
+    })
   }
 })
